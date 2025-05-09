0de780658c8bd45fcc177207c57bb5ee14e393d5
Jefferson Faria <138058131+JeffersonFaria1@users.noreply.github.com>
2025-05-09 12:43:00 +0000

    Frase pro meu AMOR

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luriane, Pérola e João. Eu amo muito vocês!!! São os amores da minha vida</h1>    
+    return <h1>Passando pra dizer que te amo, E se você está lendo essa mensagem me manda um eu te amo no Zap!</h1>    
     }
 
 export default Home;
