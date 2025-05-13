7dbf0d0049748fd662e4c27f799b41a2fea0cdab
Jefferson Faria <138058131+JeffersonFaria1@users.noreply.github.com>
2025-05-13 11:09:28 +0000

    Novo Status

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Passando pra dizer que te amo, E se você está lendo essa mensagem me manda um eu te amo no Zap!</h1>    
+    return <h1>Teste 1</h1>    
     }
 
 export default Home;
